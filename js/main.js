10da5f55af24f84b2571ccd252e3a7f7c29ced2d
PopovRY <rupopov91@gmail.com>
2021-09-28 23:15:41 +1000

    Написал 2 функции.

--- js/main.js
+++ js/main.js
@@ -6,12 +6,14 @@ function getRandomIntInclusive(min, max) {
     return;
   }
   return Math.floor(Math.random() * (max - min + 1)) + min;}
-console.log(getRandomIntInclusive(0, 50));
 
-function getRandomFloat(min, max) {
+getRandomIntInclusive(0, 50);
+
+function getRandomFloat(min, max, decimalPlaces) {
   if (min >= max) {
     console.log('Недопустимое значение диапазона');
     return;
   }
-  return Math.random() * (max - min) + min;}
-console.log(getRandomFloat(0, 200));
+  return (Math.random() * (max - min) + min).toFixed(decimalPlaces);}
+
+getRandomFloat(0, 200, 1);
