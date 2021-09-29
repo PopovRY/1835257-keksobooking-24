5968022cbd460bec673b67c6146d68b89ff47d5c
PopovRY <rupopov91@gmail.com>
2021-09-29 16:24:03 +1000

    Добавил стрелочные функции Добавил throw new Error

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-function getRandomIntInclusive(min, max) {
+const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   if (min >= max) {
-    console.log('Недопустимое значение диапазона');
-    return;
+    throw new Error('Недопустимое значение диапазона');
   }
-  return Math.floor(Math.random() * (max - min + 1)) + min;}
+  return Math.floor(Math.random() * (max - min + 1)) + min;};
 
-getRandomIntInclusive(0, 50);
+getRandomIntInclusive(0, 500);
 
-function getRandomFloat(min, max, decimalPlaces) {
+const getRandomFloat = (min, max, decimalPlaces) => {
   if (min >= max) {
-    console.log('Недопустимое значение диапазона');
-    return;
+    throw new Error('Недопустимое значение диапазона');
   }
-  return (Math.random() * (max - min) + min).toFixed(decimalPlaces);}
+  return (Math.random() * (max - min) + min).toFixed(decimalPlaces);};
 
 getRandomFloat(0, 200, 1);
