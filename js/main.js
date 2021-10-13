ebe2e3beef7472199be3f52cb2210b3c3931ce31
PopovRY <rupopov91@gmail.com>
2021-10-13 15:23:14 +1000

    Поправил import в main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {createAdvertisement} from './util';
+import {createAdvertisement} from './util.js';
 
 // Собираем массив
 
@@ -6,4 +6,5 @@ const NUMBER_OF_OBJ = 10;
 const objectGenerator = new Array(NUMBER_OF_OBJ).fill(null).map(createAdvertisement);
 
 objectGenerator;
+
 
