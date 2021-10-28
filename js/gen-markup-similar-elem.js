4b8f63838cf8031341aef4c05d6b2bd523d7397c
PopovRY <rupopov91@gmail.com>
2021-10-28 11:13:59 +1000

    Создал ветку + модуль

--- js/gen-markup-similar-elem.js
+++ js/gen-markup-similar-elem.js
@@ -1,5 +1,25 @@
-const similarListCard = document.querySelector('#map-canvas');
-const similarCardTemplate = document.querySelector('#card')
+import {createAdvertisement} from './util.js';
 
-const cardElement = similarCardTemplate.cloneNode(true);
-similarListCard.appendChild(cardElement);
+const listCard = document.querySelector('#map-canvas');
+const cardTemplate = document.querySelector('#card')
+
+const cardElement = cardTemplate.cloneNode(true);
+listCard.appendChild(cardElement);
+
+const similarAdvertisement = createAdvertisement();
+
+similarAdvertisement.forEach((author, offer, location) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__avatar').textContent = author.avatar;
+  cardElement.querySelector('.popup__title').textContent = offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
+  cardElement.querySelector('').textContent = ;
+
+
+  listCard.appendChild(cardElement);
+});
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ import {createAdvertisement} from './util.js';
 const NUMBER_OF_OBJ = 10;
 const objectGenerator = new Array(NUMBER_OF_OBJ).fill(null).map(createAdvertisement);
 
-objectGenerator;
+export {objectGenerator}
 
 
