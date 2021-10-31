a9fcf7d1b99d4ddc1e1a3e35a34e767cd494be2c
PopovRY <rupopov91@gmail.com>
2021-10-31 15:33:40 +1000

    Создал ветку + модуль

--- js/gen-markup-similar-elem.js
+++ js/gen-markup-similar-elem.js
@@ -5,43 +5,6 @@ import {
 
 
 
-
-
-
-
-const listCard = document.querySelector('#map-canvas');
-const cardTemplate = document.querySelector('#card')
-
-const cardElement = cardTemplate.cloneNode(true);
-listCard.appendChild(cardElement);
-
-const similarAdvertisements = createAdvertisement();
-
-const similarListFragment = document.createDocumentFragment();
-
-similarAdvertisements.forEach({author, offer, location}) => {
-  const cardElement = cardTemplate.cloneNode(true);
-  cardElement.querySelector('.popup__avatar').textContent = author.avatar;
-  cardElement.querySelector('.popup__title').textContent = offer.title;
-  cardElement.querySelector('.popup__text--address').textContent = offer.address;
-  cardElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
-  cardElement.querySelector('.popup__type').textContent = offer.type;
-
-
-  cardElement.querySelector('.popup__text--capacity').textContent = `Заезд после ${offer.checkin}}, выезд до ${offer.checkout}`;
-  cardElement.querySelector('.popup__features').textContent = offer.features;
-  cardElement.querySelector('.popup__description').textContent = offer.description ;
-  cardElement.querySelector('.popup__photos').textContent = offer.photos ;
-
-
-
-  similarListFragment.appendChild(cardElement);
-});
-
-listCard.appendChild(similarListFragment);
-
-
-
 const featureContainer = document.querySelector('.popup__features');
 const featureListFragment = document.createDocumentFragment();
 
@@ -55,5 +18,60 @@ FEATURES.forEach((feature) => {
 
 featureContainer.innerHTML = '';
 featureContainer.append(featureListFragment);
+
+// вспомогательные
+
+
+const translatePopupType = () => {}
+
+
+const makeCardFeatures = () => {};
+
+
+const makeCardPhotos = () => {};
+
+const checkEmptyBlock = () => {
+
+};
+
+// добавить данные в шаблон
+
+const cardTemplate = document.querySelector('#card')
+
+const adverts = createAdvertisement();
+
+const popupFragment = document.createDocumentFragment();
+
+const makeCardList = () => {
+adverts.forEach(({author, offer}) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector('.popup__title').textContent = offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = offer.address;
+  //cardElement.querySelector('.popup__text--address').textContent = '';
+  cardElement.querySelector('.popup__text--price').textContent = `${offer.price} ₽/ночь`;
+  cardElement.querySelector('.popup__type').textContent = translatePopupType(offer.type);
+  cardElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
+  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+  makeCardFeatures(cardElement, offer.features);
+  cardElement.querySelector('.popup__description').textContent = offer.description ;
+  makeCardPhotos(cardElement, offer.photos);
+  cardElement.querySelector('.popup__avatar').src = author.avatar;
+  checkEmptyBlock(cardElement);
+  popupFragment.appendChild(cardElement);
+});
+return popupFragment;
+};
+
+
+
+// отрисовать карточку
+
+const mapCanvas = document.querySelector('#map-canvas');
+const cardList = makeCardList();
+const firstCardItem = cardList.querySelectorAll('.popup');
+mapCanvas.appendChild(firstPopupItem[0]);
+
+
+
 
 
