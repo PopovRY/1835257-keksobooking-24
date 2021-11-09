036991b900c1824481f8bbebdcf5e5940d5bc694
PopovRY <rupopov91@gmail.com>
2021-11-09 18:00:52 +1000

    поправил ошибки линтера

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ import {createAdvertisement} from './util.js';
 // Собираем массив
 
 const NUMBER_OF_OBJ = 10;
-const objectGenerator = new Array(NUMBER_OF_OBJ).fill(null).map(createAdvertisement);
-const adverts = objectGenerator
-export {objectGenerator, adverts}
+const adverts = new Array(NUMBER_OF_OBJ).fill(null).map(createAdvertisement);
+
+export {adverts};
 
 
